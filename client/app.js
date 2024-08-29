94ec9d70663f61196209620604d1e1acc43d4721
rolansy <ronalshoeygeorge@gmail.com>
2024-08-29 22:22:01 +0530

    chore: Update client-side code to include functionality for estimating home prices

--- client/app.js
+++ client/app.js
@@ -16,4 +16,46 @@ function onPageLoad(){
 
 }
 
+function getBathValue(){
+    var uiBathrooms = document.getElementsByName("uiBathrooms");
+    for(var i in uiBathrooms){
+        if(uiBathrooms[i].checked){
+            return parseInt(i)+1;
+        }
+    }
+    return -1; // Invalid Value
+}
+
+function getBHKValue(){
+    var uiBHK = document.getElementsByName("uiBHK");
+    for(var i in uiBHK){
+        if(uiBHK[i].checked){
+            return parseInt(i)+1;
+        }
+    }
+    return -1; // Invalid Value
+}
+
+function onClickEstimatePrice(){
+    console.log("Estimate price button clicked");
+    var sqft = document.getElementById("uiSqft");
+    var bhk = getBHKValue();
+    var bathrooms = getBathValue();
+    var location = document.getElementById("uiLocations");
+    var estPrice = document.getElementById("uiEstimatedPrice");
+
+    var url = "http://127.0.0.1:5000/predict_home_price";
+
+    $.post(url,{
+        total_sqft: parseFloat(sqft.value),
+        bhk: bhk,
+        bath: bathrooms,
+        location: location.value
+    },function(data, status){
+        console.log(data.estimated_price);
+        estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
+        console.log(status);
+    });
+}
+
 window.onload = onPageLoad;
